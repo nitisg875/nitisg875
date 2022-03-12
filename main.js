c9450bf3c23124ad77cf5d1098a30d6db0db343d
Nitish Anand <96649552+nitisg875@users.noreply.github.com>
2022-03-13 00:19:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+switch (command) {
+    case "tree":
+        //call tree function
+        break;
+    case "organize":
+        //call organize function
+        break;
+    case "help":
+        //call help function
+        break;
+    default:
+        console.log("command not recognized :/")
+        break;
+}
